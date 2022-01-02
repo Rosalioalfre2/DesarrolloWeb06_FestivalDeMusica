61fee03fe036a58b820ba8a324899796308d9424
Rosalioalfre2 <rosalioalfre2@gmail.com>
2022-01-02 10:52:35 -0600

    Mejorando la funcion de gulp, agregando un watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 //Solicitamos la dependencia
-const { src , dest } = require('gulp');
+const { src , dest , watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function css(done){
@@ -11,7 +11,17 @@ function css(done){
             .pipe(sass()) //2. Compilarlo
             .pipe(dest('build/css')) //3. Almacenarlo
 
+
+    console.log('\nSe compilo correctamente');
+
+    done();
+}
+
+function watchSCSS(done){
+    watch('src/scss/app.scss', css);
+    console.log('Funcion watch scss');
     done();
 }
 
 exports.css = css;
+exports.watchSCSS = watchSCSS;
